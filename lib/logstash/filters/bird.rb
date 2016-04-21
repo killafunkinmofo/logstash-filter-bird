ec4df8be61a00b2f54b2e1624e261baefe388c3d
killafunkinmofo <claffin@wordpress.com>
2016-04-21 20:27:04 +0000

    initial commit

---
+++ lib/logstash/filters/bird.rb
@@ -0,0 +1,62 @@
+# encoding: utf-8
+require "socket"
+require "logstash/filters/base"
+require "logstash/namespace"
+
+# This reads BGP route data from BIRD to add to your flows
+#
+class LogStash::Filters::Bird < LogStash::Filters::Base
+  config_name "bird"
+
+  # Verifications
+  #This requires to specify logstash IP field reference as address
+  #Path to bird.ctl file, default of /var/run/bird.ctl if not specified
+  #Target field path for this data, default output is bgp.fieldname
+  config :address, :validate => :string
+  config :path, :validate => :path, :default => '/var/run/bird.ctl'
+  config :target, :validate => :string, :default => 'bgp'
+  def register
+    #Nothing
+  end # def register
+
+  def filter(event)
+    if File.exist?("#{@path}")
+      event[@target] = {} if event[@target].nil?
+      @addresstry = event.sprintf(event.sprintf(@address))
+      @s = UNIXSocket.new("#{@path}")
+      @s.puts "show route for #{@addresstry} all"
+      @logger.debug("Bird Command  - \"show route for #{@addresstry} all\"")
+      #Collect the output and watch for complete / error status codes
+      @route_output = ""
+      while (@route_output.scan(/\n0000/) == []) and (@route_output.scan(/\n0013/) == []) and (@route_output.scan(/\n[89][0-9]{3}/) == []) do
+        if File.exist?("#{@path}")
+        @route_output += @s.recvfrom(1024)[0]
+        @logger.debug("Bird response:  #{@route_output}")
+        end
+      end
+      #parse the output if successfull code '0000'
+      if @route_output.include? "\n0000"
+        @netblock = @route_output.scan(/-(\d+\.\d+\.\d+\.\d+\/\d+)/)[0][0]
+        @communities = @route_output.scan(/BGP.community: (.*)\n/)
+        @communities = "" if @communities.length == 0 or @communities[0][0].empty?
+        @communities = @communities[0][0].tr(',',':').delete ")(" if @communities != ""
+        @origin = @route_output.scan(/BGP.origin: (\w+)\n/)[0][0]
+        @origin = 'Unknown' if @origin.length == 0 or @origin.empty?
+        @aspath = @route_output.scan(/BGP.as_path: (.*)\n/)[0][0]
+        @aspath = 'Internal' if @aspath.length == 0 or @aspath.empty?
+        event[@target]["aspath"] = @aspath
+        event[@target]["origin"] = @origin
+        event[@target]["communities"] = @communities
+        event[@target]["netblock"] = @netblock
+        # filter_matched should go in the last line of our successful code
+        filter_matched(event)      
+      elsif @route_output.include? "\n9001"
+        @logger.warn("Bird error: #{@route_output.scan(/9001 (.*)/)[0][0]}")
+      elsif @route_output.include? "\n8001"
+        @logger.warn("Bird error: #{@route_output.scan(/8001 (.*)/)[0][0]}")
+      else
+        @logger.warn("Bird error: Unexpected Error")
+      end
+    end # if file exists
+  end #def filter
+end # class LogStash::Filters::Bird
---
+++ logstash-filter-bird.gemspec
@@ -0,0 +1,22 @@
+Gem::Specification.new do |s|
+  s.name = 'logstash-filter-bird'
+  s.version         = '0.9.1'
+  s.licenses = ['Apache License (2.0)']
+  s.summary = "This filter provides routing information from BIRD from an IP address lookup(BGP Community, ASPATH, MED, Netblock). Meant to be added to netflow data, but can be used on any data that has an IP address"
+  s.description = "This gem is a logstash plugin required to be installed on top of the Logstash core pipeline using $LS_HOME/bin/plugin install gemname. This gem is not a stand-alone program"
+  s.authors = ["Elastic"]
+  s.email = 'info@elastic.co'
+  s.homepage = "http://www.elastic.co/guide/en/logstash/current/index.html"
+  s.require_paths = ["lib"]
+
+  # Files
+  s.files = Dir['lib/**/*','spec/**/*','vendor/**/*','*.gemspec','*.md','CONTRIBUTORS','Gemfile','LICENSE','NOTICE.TXT']
+  # Tests
+  s.test_files = s.files.grep(%r{^(test|spec|features)/})
+  # Special flag to let us know this is actually a logstash plugin
+  s.metadata = { "logstash_plugin" => "true", "logstash_group" => "filter" }
+
+  # Gem dependencies
+  s.add_runtime_dependency "logstash-core", ">= 2.0.0", "< 3.0.0"
+  s.add_development_dependency 'logstash-devutils'
+end
